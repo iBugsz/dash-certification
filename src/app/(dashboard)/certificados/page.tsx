bd3da0c17fa4d9ca7210db41130ca78ebc8b8b52
iBugsz <ngarciab357@gmail.com>
2026-08-23 15:16:30 -0500

    ....................

--- src/app/(dashboard)/certificados/page.tsx
+++ src/app/(dashboard)/certificados/page.tsx
@@ -73,7 +73,7 @@ export default function CertificatesPage() {
     const url = window.URL.createObjectURL(wordGenerated);
     const link = document.createElement("a");
     link.href = url;
-    link.download = `Certificado_${selectedCompany?.name.replace(/\s+/g, "_") || "Documento"}.docx`;
+    link.download = `Explicacion Centro Gravedad") || "Documento"}.docx`;
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
